ef695ebbd31c5f92fc138e949cd30552066ba6aa
Nelson V <nelson.avelez@hotmail.com>
2022-03-24 18:02:39 -0500

    Cambio daño la funcion principal

--- models/model.py
+++ models/model.py
@@ -52,12 +52,12 @@ class SaleOrder(models.Model):
         res = super(SaleOrder, self.with_context(from_so=self.id)).action_confirm()
         return res
 
-    def action_confirm(self):
+    '''def action_confirm(self):
         super(SaleOrder, self).action_confirm()
         if self.state == 'sale':
             if not self.analytic_account_id:
                 raise UserError(
-                    "Please add Analytic Account on all Sales Lines, in order to confirm invoice!")
+                    "Please add Analytic Account on all Sales Lines, in order to confirm invoice!")'''
 
 
     def action_post_sale(self):
